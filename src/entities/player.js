123c4f6ae8e932cf8387f998865f7570d21685ca
Amartin35 <achillemartin35pro@gmail.com>
2023-03-25 14:13:54 +0100

    ok

--- src/entities/player.js
+++ src/entities/player.js
@@ -1,36 +1,24 @@
-
 export default class Player extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y, texture) {
         super(scene, x, y, texture); 
         this.clavier = scene.input.keyboard.createCursorKeys();
         
-        
-        scene.physics.world.enable(this)
-        scene.add.existing(this)
+        scene.physics.world.enable(this);
+        scene.add.existing(this);
         this.setCollideWorldBounds(true);
         
-
-    }
-    
-    
-    create(){
-
-        
-    }
-    
-    
-    update(){
-
-        var mouvement = new Phaser.Math.Vector2(0, 0)
         // Propriétés du dash
         this.dashTime = 0;
         this.isDashing = false;
         this.dashCooldown = 0;
+        this.dashCooldownMax = 1000;
         this.dashDuration = 200;
         this.dashSpeed = 1000;
-        
-        
-        
+    }
+    
+    update(time, delta) {
+        var mouvement = new Phaser.Math.Vector2(0, 0);
+
         if (this.clavier.left.isDown) {
             mouvement.x = -1;
         } 
@@ -51,62 +39,62 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
             mouvement.y = 0;
         }
         
+        mouvement.normalize();
+        this.setVelocity(mouvement.x * PLAYER_SPEED, mouvement.y * PLAYER_SPEED);
 
 
-
+        console.log(this.isDashing);
+        // Gestion du dash
+        if (this.clavier.space.isDown && this.dashCooldown <= 0 && this.isDashing == false) {
+            this.isDashing = true;
+            this.dashTime = 0;
+            this.setVelocityX(this.dashSpeed * (mouvement.x !== 0 ? mouvement.x : this.flipX ? -1 : 1));
+        }
 
         // Vérifie si le joueur est en train de dasher
-        if (isDashing) {
+        if (this.isDashing) {
             // Met à jour le temps de dash et détermine s'il est terminé
-            this.dashTime += game.time.elapsedMS;
+            this.dashTime += 1;
+            console.log(this.dashTime);
             if (this.dashTime >= this.dashDuration) {
                 this.isDashing = false;
                 this.dashTime = 0;
                 this.dashCooldown = this.dashCooldownMax;
             }
         }
+
         // Vérifie si le dash est prêt à être utilisé à nouveau
         if (this.dashCooldown > 0) {
-            this.dashCooldown -= game.time.elapsedMS;
+            this.dashCooldown -= 1;
         }
         
         // Mouvement du joueur
-        if (cursors.left.isDown) {
-            this.setVelocityX(-160);
-
-            if (!this.isDashing) {
+        if (!this.isDashing) {
+            if (mouvement.x < 0) {
+                this.setVelocityX(-PLAYER_SPEED);
                 this.flipX = true;
             }
-        } else if (cursors.right.isDown) {
-            this.setVelocityX(160);
-
-            if (!this.isDashing) {
+            else if (mouvement.x > 0) {
+                this.setVelocityX(PLAYER_SPEED);
                 this.flipX = false;
             }
-        } else {
-            this.setVelocityX(0);
+            else {
+                this.setVelocityX(0);
+            }
+
+            if (mouvement.y < 0) {
+                this.setVelocityY(-PLAYER_SPEED);
+            }
+            else if (mouvement.y > 0) {
+                this.setVelocityY(PLAYER_SPEED);
+            }
+            else {
+                this.setVelocityY(0);
+            }
         }
         
-        if (cursors.up.isDown) {
-            this.setVelocityY(-350);
-        }
-        
-        // Gestion du dash
-        if (cursors.space.isDown && this.dashCooldown <= 0) {
-            this.isDashing = true;
-            this.setVelocityX(this.dashSpeed * (this.flipX ? -1 : 1));
-        }
-    
-    
-    
-    
-    
-    
-        mouvement.normalize();
-        
-        
-        this.setVelocity(mouvement.x*PLAYER_SPEED, mouvement.y*PLAYER_SPEED);
         this.x = Math.round(this.x);
         this.y = Math.round(this.y);
     }
 }
+
